b9648757ec1f8e783ad1bacbcb4d74432fdc0e59
Gentlejian <1515374978@qq.com>
2020-02-29 15:55:35 +0800

    完成首页展示打广告位

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -1,7 +1,10 @@
 package com.taotao.content.service.impl;
 
 import java.util.Date;
+import java.util.List;
 
+import com.taotao.pojo.TbContentExample;
+import com.taotao.pojo.TbContentExample.Criteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +28,19 @@ public class ContentServiceImpl implements ContentService {
 		//插入到内容表
 		contentMapper.insert(content);
 		return TaotaoResult.ok();
+	}
+
+
+	/**根据分类id得到content列表 */
+	@Override
+	public List<TbContent> getContentByCid(long cid) {
+		TbContentExample example = new TbContentExample();
+		Criteria criteria =example.createCriteria();
+		//设置查询条件
+		criteria.andCategoryIdEqualTo(cid);
+		//执行查询
+		List<TbContent> list = contentMapper.selectByExample(example);
+		return list;
 	}
 
 }
