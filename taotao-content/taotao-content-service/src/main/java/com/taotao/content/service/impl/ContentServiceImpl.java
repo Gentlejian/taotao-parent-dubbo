4d1ac96c52e8baf872a3d040c0c271a844884064
Gentlejian <1515374978@qq.com>
2020-02-29 20:39:01 +0800

    添加缓存

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -3,9 +3,13 @@ package com.taotao.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.jedis.JedisClient;
 import com.taotao.pojo.TbContentExample;
 import com.taotao.pojo.TbContentExample.Criteria;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.TaotaoResult;
@@ -19,6 +23,14 @@ public class ContentServiceImpl implements ContentService {
 	@Autowired
 	private TbContentMapper contentMapper;
 
+	@Autowired
+	private JedisClient jedisClient;
+
+	@Value("${INDEX_CONTENT}")
+    private String INDEX_CONTENT;
+
+
+
 	/**  新增内容管理目录  */
 	@Override
 	public TaotaoResult addContent(TbContent content) {
@@ -31,15 +43,40 @@ public class ContentServiceImpl implements ContentService {
 	}
 
 
-	/**根据分类id得到content列表 */
+	/**
+     * 根据分类id得到content列表
+     * 添加Redis缓存（使用单机版）
+     * */
 	@Override
 	public List<TbContent> getContentByCid(long cid) {
+	    //先查询缓存
+        //添加缓存不能影响正常业务逻辑
+        try {
+            //查询缓存
+            String json = jedisClient.hget(INDEX_CONTENT,cid+ "");
+            //查询到结果，把json转换成list返回
+            if(StringUtils.isNotBlank(json)) {
+                List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+                return list;
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        //缓存中没有应用，需要查询数据库
 		TbContentExample example = new TbContentExample();
 		Criteria criteria =example.createCriteria();
 		//设置查询条件
 		criteria.andCategoryIdEqualTo(cid);
 		//执行查询
 		List<TbContent> list = contentMapper.selectByExample(example);
+		//把结果添加到缓存
+        try {
+            //从数据库拿到结果添加有问题也不能影响正产业务逻辑
+            jedisClient.hset(INDEX_CONTENT,cid + "" , JsonUtils.objectToJson(list));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        //返回结果
 		return list;
 	}
 
---
+++ taotao-content/taotao-content-service/src/test/java/com/taotao/jedis/TestJedis.java
@@ -0,0 +1,53 @@
+package com.taotao.jedis;
+
+import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author 张剑锋
+ * @date 2020/2/29 - 15:31
+ *
+ * Jedis测试
+ */
+public class TestJedis {
+
+    @Test
+    public void testJedis() throws Exception {
+        //创建一个jedis对象，需要指定服务的id和端口
+        Jedis jedis = new Jedis("127.0.0.1",6379);
+        //直接操作数据库
+        jedis.set("zhang","1234");
+        String result = jedis.get("zhang");
+        System.out.println(result);
+        System.out.println(jedis);
+        //关闭jedis
+        jedis.close();
+    }
+
+    /*@Test
+    public void testJedisCluster() throws Exception {
+        //创建一个JedisCluster对象，构造参数Set类型，集合中每个元素是HostAndPort类型
+        Set<HostAndPort> nodes = new HashSet<>();
+        //向集合中添加节点
+        nodes.add(new HostAndPort("192.168.25.128",7001));
+        nodes.add(new HostAndPort("192.168.25.128",7002));
+        nodes.add(new HostAndPort("192.168.25.128",7003));
+        nodes.add(new HostAndPort("192.168.25.128",7004));
+        nodes.add(new HostAndPort("192.168.25.128",7005));
+        nodes.add(new HostAndPort("192.168.25.128",7006));
+        JedisCluster jedisCluster = new JedisCluster(nodes);
+        System.out.println(jedisCluster);
+        //使用JedisCluster操作Redis，自带连接池。JedisCluster可以是单例的
+        jedisCluster.set("cluster-test","hello jedis cluster");
+       *//* String result = jedisCluster.get("cluster-test");
+        System.out.println(result);*//*
+        //系统关闭前，关闭JedisCluster
+        jedisCluster.close();
+    }*/
+
+}
